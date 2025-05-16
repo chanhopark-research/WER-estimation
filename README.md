d1621d49d6f6f03efc2e3b90c7fbc74282a30738
acp20cp <cpark12@sheffield.ac.uk>
2025-05-16 20:43:44 +0000

    Feature extraction

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # WER-estimation
 Implementation of Error Rate estimation models from research papers
 
-# Task List (March 2025)
-- [ ] List datasets that can be downloaded from PyTorch/Hugging Face
-- [ ] Create a script for downloading and preprocessing datasets
+# Task List
+- [x] Download TEDLIUM3 using torchaudio
+- [ ] Feature extraction (HuBERT, XLM-R)
 - [ ] Transcribe speech using Automatic Speech Recognitioni (ASR) models (Whisper/HuBERT)
 - [ ] Generate labels for Character Error Rate (CER) and Word Error Rate (WER)
-- [ ] Develop a script for feature extraction (HuBERT/XLS-R, RoBERTa/XLM-R)
 - [ ] Train a CER/WER estimation model
 - [ ] Evaluate the model
+
+# datasets
+- dev: 591 segments/1.71 hours
+- test: 1469 segments/3.05 hours
+- train: 268263 segments/453.81 hours
---
+++ scripts/feature_extraction.py
@@ -0,0 +1,198 @@
+import argparse
+import yaml
+import logging
+import torchaudio
+from pathlib import Path
+import os
+import numpy
+from kaldiio import WriteHelper
+import torch
+from torch.utils.data import Dataset, DataLoader
+import fairseq
+import sys
+
+#==================================================================
+# set logger
+#==================================================================
+logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+#==================================================================
+# set device
+#==================================================================
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+logging.info(f'device: {device}')
+
+#==================================================================
+# set parameters
+#==================================================================
+total_jobs = 10 # the number of parallel jobs
+job_number = 1  
+dataset_name = 'tl3_dev' # tl3_dev tl3_test tl3_train
+feature_name = 'hubert' # audio: hubert, wavlm, xlsr-53, data2vec / text: roberta, deberta-v3, gpt2, xlmr
+training_type = 'ft' # ft for fine-tuned, pt for pre-trained
+feature_layer = 24 # 1-based integer
+feature_level = 'sequence' # sequence for sequence-level, othewise frame-level
+feature_size = 'large' # additional information for a model's name
+
+encoder_name = f'{feature_name}-{training_type}-{feature_layer}-{feature_level}.{feature_size}'
+
+#==================================================================
+# global variables
+#==================================================================
+if dataset_name.startswith('swb'):
+    SAMPLE_RATE = 8000
+else:
+    SAMPLE_RATE = 16000
+
+#==================================================================
+# build a segment list from stm and audmap files
+#==================================================================
+def get_id(meta_info):
+    return meta_info.strip().split(',')[0][4:]
+
+def build_segment_dict(dataset_name):
+    # list of utterance_dict and transcripts
+    utterance_dict = dict()
+    transcript_dict = dict()
+
+    dataset_path = f'/share/mini1/res/t/asr/multi/multi-en/acsw/selft/datasets/{dataset_name}'
+    audmap_file_full_path = f'{dataset_path}/data.audmap'
+    stm_file_full_path = f'{dataset_path}/data.stm'
+
+    # read an audmap file
+    with open(audmap_file_full_path, 'r') as audmap_file:
+        logging.info(f'audmap_file_full_path: {audmap_file_full_path}')
+        for line in audmap_file:
+            splited = line.strip().split()
+            logging.debug(f'splited: {splited}')
+            stm_id = splited[0]
+            channel = int(splited[1]) - 1
+            start_time = float(splited[2])
+            end_time = float(splited[3])
+            # remove utterances shorter than 0s
+            if end_time < start_time:
+                logging.info(f'{splited[0]} is shorter than 0')
+                continue
+            utterance_dict[stm_id] = dict()
+            utterance_dict[stm_id]['channel'] = channel
+            utterance_dict[stm_id]['start_time'] = start_time
+            utterance_dict[stm_id]['end_time'] = end_time
+            if dataset_name.startswith('wsj'):
+                utterance_dict[stm_id]['full_path'] = f'/share/mini1/res/t/asr/multi/multi-en/acsw/selft/wavfiles/{dataset_name}/{stm_id[:12]}/{stm_id}.wav'
+            else:
+                utterance_dict[stm_id]['full_path'] = splited[-1]
+
+    # read a stm file
+    with open(stm_file_full_path, 'r') as stm_file:
+        logging.info(f'stm_file_full_path: {stm_file_full_path}')
+        for line in stm_file:
+            splited = line.strip().split()
+            stm_id = f'{splited[0]}_{get_id(splited[5])}'
+            logging.debug(f'stm_id: {stm_id}')
+            transcript_dict[stm_id] = (' ').join(splited[6:])
+
+    # common stm_ids
+    stm_ids = set(utterance_dict.keys()) & set(transcript_dict.keys())
+    return list(stm_ids), utterance_dict, transcript_dict
+
+#==================================================================
+# load models
+#==================================================================
+# audio: hubert
+if feature_name == 'hubert' and training_type == 'pt' and feature_size == 'large':
+    bundle = torchaudio.pipelines.HUBERT_LARGE
+    feature_encoder = bundle.get_model()
+elif feature_name == 'hubert' and training_type == 'ft' and feature_size == 'large':
+    bundle = torchaudio.pipelines.HUBERT_ASR_LARGE
+    feature_encoder = bundle.get_model()
+# text: xlmr
+elif feature_name == 'xlmr' and training_type == 'pt' and feature_size == 'large':
+    feature_encoder = torch.hub.load('pytorch/fairseq', 'xlmr.large')
+
+feature_encoder.to(device).eval()
+logging.info(f'feature_encoder: {feature_encoder}')
+
+#=================================================================
+# convert utterance to waveform
+#==================================================================
+def convert_utterance_to_waveform(utterance):
+    channel    = utterance['channel']
+    start_time = utterance['start_time']
+    end_time   = utterance['end_time']
+    full_path  = utterance['full_path']
+
+    frame_offset = int(start_time * SAMPLE_RATE)
+    num_frames = int(end_time * SAMPLE_RATE) - frame_offset
+
+    if full_path.endswith('sph'): # switchboard
+        waveform, sample_rate = torchaudio.backend.sox_io_backend.load(filepath=full_path, frame_offset=frame_offset, num_frames=num_frames, format='sph')
+    elif full_path.endswith('flac'): # librispeech
+        waveform, sample_rate = torchaudio.backend.sox_io_backend.load(filepath=full_path, frame_offset=frame_offset, num_frames=num_frames, format='flac')
+    else:
+        waveform, sample_rate = torchaudio.backend.sox_io_backend.load(filepath=full_path, frame_offset=frame_offset, num_frames=num_frames, format='wav')
+    waveform = waveform[channel]
+
+    assert sample_rate == SAMPLE_RATE, 'sample_rate is different from SAMPLE_RATE'
+
+    if sample_rate != 16000:
+        waveform = torchaudio.functional.resample(waveform, sample_rate, 16000)
+    return waveform
+
+#==================================================================
+# split the list for parallel jobs
+#==================================================================
+def split_segment_for_parallel(stm_ids):
+    total_lines = len(stm_ids)
+    if total_jobs > 0:
+        quotient = total_lines // total_jobs
+        remainder = total_lines % total_jobs
+        if remainder > 0:
+            quotient += 1
+        start_line = quotient * (job_number - 1)
+        end_line = quotient * job_number
+        if end_line > total_lines:
+            end_line = total_lines
+    else:
+        start_line = 0
+        end_line = total_lines
+    logging.info(f"start_line: {start_line}, end_line: {end_line}, total_lines: {total_lines}")
+    return start_line, end_line
+
+#==================================================================
+# save waveform embeddings - ark,scp
+#==================================================================
+def save_features_ark_scp(dataset_name, encoder_name, feature_layer = int(-1), feature_level='sequence'):
+    stm_ids, utterance_dict, transcript_dict = build_segment_dict(dataset_name)
+    stm_ids.sort()
+    start_line, end_line = split_segment_for_parallel(stm_ids)
+    logging.info(f"save {encoder_name} features - ark,scp: {len(stm_ids)} segments")
+
+    output_dir = f"/share/mini1/res/t/asr/multi/multi-en/acsw/selft/features/{dataset_name}"
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    ark_file_name = f"{output_dir}/{encoder_name}.ark"
+    scp_file_name = f"{output_dir}/{encoder_name}.scp"
+
+    with WriteHelper(f'ark,scp:{ark_file_name},{scp_file_name}') as writer:
+        for key in stm_ids[start_line:end_line]:
+            logging.info(f'key: {key}')
+            # audio: hubert
+            if encoder_name.startswith('hubert'):
+                waveform = convert_utterance_to_waveform(utterance_dict[key])
+                features, _ = feature_encoder.extract_features(waveform[None, :].to(device)) # [layer][batch_size, frames, 1024]
+                features = features[feature_layer - 1]
+            # text: xlmr
+            elif encoder_name.startswith('xlmr'):
+                tokens = feature_encoder.encode(transcript_dict[key])
+                features = feature_encoder.extract_features(tokens.to(device))
+            else:
+                logging.error('ERROR: unknown encoder: {encoder_name}')
+            # aggregate raw features
+            if feature_level == 'sequence':
+                features = torch.mean(features, dim=1)  # (1, 1024)
+            else:
+                features = features[0]
+            logging.info(f'features.shape: {features.shape}')
+            # save features
+            writer(key, features.cpu().detach().numpy())
+
+save_features_ark_scp(dataset_name, encoder_name, feature_layer, feature_level)
